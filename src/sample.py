16cbf0933789314be46401e8fa34508f35951185
m_hikichi <hijiki.mshr19980119@outlook.com>
2024-04-14 09:58:41 +0900

    feat: 設定したシーンをレンダリングするためのサンプルプログラムの追加

--- src/sample.py
+++ src/sample.py
@@ -2,7 +2,8 @@ import mitsuba as mi
 
 mi.set_variant('scalar_rgb')
 
-scene = mi.load_dict(mi.cornell_box())
+# scene = mi.load_dict(mi.cornell_box())
+scene = mi.load_file("/workspace/scene/scenes/cbox.xml")
 
 image = mi.render(scene, spp=128)
 
